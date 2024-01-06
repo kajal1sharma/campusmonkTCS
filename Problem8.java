49b3f6cbda4ee3b4abde7a51d46ef836fb888137
kajal <kajal@gmail.com>
2024-01-06 22:37:54 +0530

    ..

--- Problem8.java
+++ Problem8.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class Problem8 {
     public static void main(String[] args) {
-        int n=9876;
+         int n=9876;
 
-        int rev=0;
+        int evenSum=0;
         while(n!=0){
              int digit = n%10;
-             rev= rev*10+digit;
+             if(digit%2==0){
+                evenSum+=digit;
+             }
              n=n/10;
         }
-        System.out.println(rev);
+        System.out.println(evenSum);
+        // int rev=0;
+        // while(n!=0){
+        //      int digit = n%10;
+        //      rev= rev*10+digit;
+        //      n=n/10;
+        // }
+        // System.out.println(rev);
         //int ans= 6789;
 
 
